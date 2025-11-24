273321ca3929670afc3e57bf8c3481da431aef75
ducftfe8910-lgtm <ducftfe8910@gmail.com>
2025-11-24 21:37:55 +0700

    Add files via upload

---
+++ buoi_2.c
@@ -0,0 +1,47 @@
+// Commment nghia la khong cho trinh bien dich dich dong code nay
+// Day la tien xu ly (preprocessor). Muc dich la nhung thu vien 
+//Standard Input Output (Thu vien nhap xuat chuan) vao trong file thuc thi
+#include  <stdio.h>
+
+
+// Day la ham ma chuong trinh se thuc dau tien
+int main(){
+	// day la than ham
+	
+	//Ham printf ( print fomatted) dung de in ra man hinh mot cai gi do
+	printf("Xin chao \n");
+	
+	int tuoi = 0;
+	// %d, %f,% lf, %c .. goi laf format 
+	printf("Tuoi cua toi la: %d \n", tuoi);
+	
+	float diemToan = 4.5;
+	float diemVan = 7.5;
+	float diemAnh = 5.5;
+	
+	printf("Diem toan: %lf\n", diemToan);
+	printf("Diem van: %lf\n", diemVan);
+	printf("Diem anh: %lf \n", diemAnh);
+	
+
+
+	
+	char kyTu = 'A';
+	printf("Gia tri cua bien kyTu la: %c", kyTu);
+	
+	// De nhap du lieu tu ban phims dung ham sacnf()
+	int a = 0;
+	printf("Nhap so a:");
+	// Dau & la toan tu dia chi dung de xacs dinh gia tri cua bien a duoc luu o  dau cua RAM
+	scanf("%d", &a);
+	
+	printf("Gia tri cua bien a la: %d /n", a);
+	
+	int b = 0;
+	printf("Nhap so b:");
+	scanf("%d", &b);
+	printf("Gia tri cua bien b la %d", b);
+	
+	int tong = a 
+	return 0;
+}
