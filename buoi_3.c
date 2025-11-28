dd2fcc39aa6d94cce8b49bc3436e77a0a5f3f597
ducftfe8910-lgtm <ducftfe8910@gmail.com>
2025-11-28 21:44:41 +0700

    Add files via upload

---
+++ buoi_3.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	
+	
+	
+//	int a;
+//	int b;
+//	
+//	
+//	
+//	printf("Nhap so a:");
+//	scanf("%d", &a);
+//	
+//	
+//	printf("Nhap so b:");
+//	scanf("%d", &b);
+//	
+//	
+//	
+//	printf("a - b = %d", a -b);
+//	
+//	printf("a * b = %d", a * b);
+//	
+//	
+//	
+//	
+	
+//	int a, b;
+//	
+//	printf("Nhap so a: ");
+//	scanf("%d", &a);
+//	
+//	printf("Nhap so b: ");
+//	scanf("%d", &b);
+//	
+//	printf("a / b = %.1f", (float)a /b);
+//	
+//	printf("\n");
+// toan tu (operators) + - * /
+// toan hang
+//	
+//	printf("Gia tri cua bien a: %d \n", a);
+//	printf("Dia chi cua bien a trong RAM là: %p", &a);
+//	
+	
+//	int a = 1, b = 2;
+	
+//	printf("Phep du lay du cua a va b la: %d", a % b);
+	
+//	printf("%d + %d =%d", a, b, a + b);
+//	
+//	printf("%d chia lay du cho %d la %d", a, b, a % b);
+	
+	
+//	int a = 5;
+//	printf("Gia tri cua bien a la: %d \n", a);
+//	a++;
+//	a = a + 1;
+//	printf("Gia tri cua bien a sau thay doi: %d \n", ++a);
+//	printf("Gia tri cua bien a la: %d", a);
+//	
+	
+//	int a = 5;
+//	a = --a + 1 + a--;
+//	printf("Gia tri cua a la: %d", a);
+//	
+	//kiem tra a có bàng 5 hay khong thi ==
+	
+	// tu phien ban C99kieu dı lieu boolean nhung ko chinh thuc 
+//	
+//	int a = 5;
+//	printf("a += 5 la: %d", a +=5);
+//	
+//	printf("a -= 5 la: %d", a -=5);
+//	
+//	printf("a *= 5 la: %d", a *=5);
+//	
+//	printf("a /= 5 la: %d", a /=5);
+//	
+//	printf("a chia lay phan du cho 5 la: %d", a %=5);
+//	
+	
+	
+	int x = 5, y = 3;
+	printf("x > y: %d \n:", x > y);
+	
+	printf("x > y: %d \n:", x < y);
+	
+	printf("x = y: %d \n:", x ==y);
+	
+	
+	printf("x >=y: %d \n:", x > y);
+	
+
+		
+	
+	
+
+
+
+
+
+          
+
+
+
+
+
+	
+	
+	
+	
+	
+	return 0;
+}
